079ce5034b4eb4d5c105e8c09ee5ef09eb723d6c
venureddyy <venureddyy@outlook.com>
2026-01-15 04:08:13 +0000

    addes slides

--- script.js
+++ script.js
@@ -16,3 +16,31 @@ setTimeout(() => {
   if (splash) splash.style.display = "none";
 }, 1000); // 1s video + 1s fade
 
+
+// Slide Show
+let currentSlide = 0;
+
+function showSlide(n) {
+  const slides = document.getElementsByClassName("slide");
+  const dots = document.getElementsByClassName("dot");
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+    dots[i].classList.remove("active");
+  }
+
+  slides[n].style.display = "block";
+  dots[n].classList.add("active");
+}
+
+// Initial display
+document.addEventListener("DOMContentLoaded", () => {
+  showSlide(currentSlide);
+
+  // Auto-slide every 5 seconds
+  setInterval(() => {
+    currentSlide = (currentSlide + 1) % document.getElementsByClassName("slide").length;
+    showSlide(currentSlide);
+  }, 5000);
+});
+
